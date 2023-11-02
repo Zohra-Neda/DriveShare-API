cb029769d6b16ec660772bea6b1798a5583c5a4c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-11-02 14:29:33 +0200

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_26_130051) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_122727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_26_130051) do
     t.datetime "updated_at", null: false
     t.bigint "city_id", null: false
     t.bigint "user_id", null: false
+    t.decimal "price"
+    t.string "model"
     t.index ["city_id"], name: "index_cars_on_city_id"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
