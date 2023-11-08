c308e9bbffde062b258f635dd7161e6a2b0fcd53
zohra-neda <zohra.neda@gmail.com>
2023-11-08 19:08:23 +0430

    Update render-build.sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# Disable database environment check
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # exit on error
 set -o errexit
 
@@ -6,5 +9,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
